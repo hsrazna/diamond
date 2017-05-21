0f55c8aba9473ee7634c49004bb9ae02efa62c84
hsrazna <anzar.terek@gmail.com>
2017-05-21 15:01:42 +0300

    header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,20 @@
 <header class="header">
-	<div class="wrap">
-		
+	<div class="bgT"></div>
+	<div class="logo">
+		<img src="css/images/logo.png" alt="" title="">
+	</div>
+	<div class="wrapp">
+		<nav class="menu">
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">ABOUT US</a></li>
+				<li><a href="#">SIGN UP</a></li>
+				<li><a href="#">RULES</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">MONITORS</a></li>
+				<li><a href="#">BANNERS</a></li>
+				<li><a href="#">CONTACTS</a></li>
+			</ul>
+		</nav>
 	</div>
 </header>
